e6c6fe159de5927071be54aca92529b5cd670894
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-01 20:40:31 +0200

    Moved UserDetails implementation to UserPrincipal from UserResponse.

--- src/main/java/com/coinverse/api/common/models/UserResponse.java
+++ src/main/java/com/coinverse/api/common/models/UserResponse.java
@@ -4,57 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-public class UserResponse implements UserDetails {
+public class UserResponse {
     private Long id;
     private String firstName;
     private String lastName;
     private String emailAddress;
     private String roleName;
-    private String passwordHash;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(roleName));
-    }
-
-    @Override
-    public String getPassword() {
-        return passwordHash;
-    }
-
-    @Override
-    public String getUsername() {
-        return emailAddress;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
