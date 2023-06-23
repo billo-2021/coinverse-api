7f584fcc6f554e233a26301304e97ad17c7a3664
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-23 18:58:16 +0200

    Added user service.

--- src/main/java/com/coinverse/api/common/services/UserServiceImpl.java
+++ src/main/java/com/coinverse/api/common/services/UserServiceImpl.java
@@ -1,91 +1,152 @@
 package com.coinverse.api.common.services;
 
-import com.coinverse.api.common.entities.User;
-import com.coinverse.api.common.entities.UserRole;
+import com.coinverse.api.common.entities.*;
+import com.coinverse.api.common.exceptions.InvalidRequestException;
 import com.coinverse.api.common.mappers.UserMapper;
-import com.coinverse.api.common.models.UserRequest;
-import com.coinverse.api.common.models.UserResponse;
-import com.coinverse.api.common.repositories.UserRepository;
-import com.coinverse.api.common.repositories.UserRoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.coinverse.api.common.models.*;
+import com.coinverse.api.common.repositories.*;
+import com.coinverse.api.common.validators.UserEmailAddressRequestValidator;
+import com.coinverse.api.common.validators.UserPreferenceUpdateRequestValidator;
+import com.coinverse.api.common.validators.UserRequestValidator;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-    private final UserRoleRepository userRoleRepository;
+    private final AccountRepository accountRepository;
+    private final AddressRepository addressRepository;
+    private final UserPreferenceRepository userPreferenceRepository;
+    private final WalletRepository walletRepository;
+    private final CryptoCurrencyRepository cryptoCurrencyRepository;
+    private final CryptoCurrencyKeyGenerator cryptoCurrencyKeyGenerator;
     private final UserMapper userMapper;
-
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository, UserRoleRepository userRoleRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userRoleRepository = userRoleRepository;
-        this.userMapper = userMapper;
-    }
+    private final UserRequestValidator userRequestValidator;
+    private final UserEmailAddressRequestValidator userEmailAddressRequestValidator;
+    private final UserPreferenceUpdateRequestValidator userPreferenceUpdateRequestValidator;
 
     @Override
-    public Optional<UserResponse> getUserById(Long id) {
-        Optional<User> userResponse = userRepository.findById(id);
+    public Optional<UserResponse> getUserById(@NotNull final Long id) {
+        final Optional<User> userResponse = userRepository.findById(id);
         return userResponse.map(userMapper::userToUserResponse);
     }
 
     @Override
-    public Optional<UserResponse> getUserByEmailAddress(String emailAddress) {
-        Optional<User> userResponse = userRepository.findByEmailAddress(emailAddress);
+    public Optional<UserResponse> getUserByEmailAddress(@NotNull final String emailAddress) {
+        final Optional<User> userResponse = userRepository.findByEmailAddress(emailAddress);
+        return userResponse.map(userMapper::userToUserResponse);
+    }
+
+    @Override
+    public Optional<UserResponse> getUserByAccountId(@NotNull final Long accountId) {
+        final Optional<User> userResponse = userRepository.findByAccountId(accountId);
         return userResponse.map(userMapper::userToUserResponse);
     }
 
     @Override
     public List<UserResponse> getUsers() {
-        List<User> users = userRepository.findAll();
+        final List<User> users = userRepository.findAll();
         return userMapper.usersToUserResponses(users);
     }
 
+    @Transactional
     @Override
-    public UserResponse addUser(UserRequest userRequest) {
-        final String roleName = userRequest.getRoleName();
-        final Optional<UserRole> userRoleResponse = userRoleRepository.findByName("admin");
+    public UserResponse addUser(@NotNull final UserRequest userRequest) {
+       final User user = userRequestValidator.validate(userRequest);
 
-        if (userRoleResponse.isEmpty()) {
-            throw new RuntimeException("User role with " + "admin" + " does not exist");
-        }
+       final Account account = user.getAccount();
+       accountRepository.save(account);
 
-        UserRole userRole = userRoleResponse.get();
-        User user = userMapper.userRequestToUser(userRequest);
-        user.setRole(userRole);
-        User savedUser = userRepository.save(user);
+       final Address address = user.getAddress();
+       addressRepository.save(address);
 
-        return userMapper.userToUserResponse(user);
+       final UserPreference userPreference = user.getPreference();
+       userPreferenceRepository.save(userPreference);
+
+       final User savedUser = userRepository.save(user);
+
+        final List<CryptoCurrency> walletCurrencies = cryptoCurrencyRepository.findAll();
+        final List<Wallet> wallets = walletCurrencies
+                .stream()
+                .map(walletCurrency -> {
+                    final CryptoCurrencyKey currencyKey = cryptoCurrencyKeyGenerator.generate();
+
+                    return Wallet.
+                            builder()
+                            .account(account)
+                            .currency(walletCurrency)
+                            .privateKey(currencyKey.getPrivateKey())
+                            .publicKey(currencyKey.getPublicKey())
+                            .address(currencyKey.getAddress())
+                            .balance(BigDecimal.valueOf(0))
+                            .build();
+                        }
+                ).toList();
+        walletRepository.saveAll(wallets);
+
+       return userMapper.userToUserResponse(savedUser);
     }
 
     @Override
-    public UserResponse updateUser(Long id, User user) {
-        var dbUserResult = userRepository.findById(id);
+    public UserResponse updateUser(@NotNull final Long id, @NotNull final UserRequest userRequest) {
+        final User existingUser = userRepository.findById(id).orElseThrow(
+                () -> new InvalidRequestException("Invalid user id '" + id + "'")
+        );
 
-        if (dbUserResult.isEmpty()) {
-            throw new RuntimeException("User with id " + id + " does not exist");
-        }
+        existingUser.setFirstName(userRequest.getFirstName());
+        existingUser.setLastName(userRequest.getLastName());
 
-        var dbUser = dbUserResult.get();
-        dbUser.setFirstName(user.getFirstName());
-        dbUser.setLastName(user.getLastName());
-
-        User savedUser = userRepository.save(dbUser);
+        final User savedUser = userRepository.save(existingUser);
         return userMapper.userToUserResponse(savedUser);
     }
 
+    @Transactional
     @Override
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+    public void updateUserByEmailAddress(@NotNull String emailAddress, @NotNull UserUpdateRequest userUpdateRequest) {
+        String phoneNumber = userUpdateRequest.getPhoneNumber();
+        UserPreferenceUpdateRequest userPreferenceUpdateRequest = userUpdateRequest.getPreference();
+
+        User user = userEmailAddressRequestValidator.validate(emailAddress);
+
+        if (phoneNumber != null) {
+            user.setPhoneNumber(phoneNumber);
+        }
+
+        UserPreference userPreference = userPreferenceUpdateRequestValidator.validate(userPreferenceUpdateRequest);
+        updateUserPreference(user, userPreference);
+
+        userRepository.save(user);
+    }
+
+    private void updateUserPreference(User user, UserPreference preference) {
+        UserPreference userPreference = user.getPreference();
+
+        if (preference == null) {
+            return;
+        }
+
+        Currency preferredCurrency = preference.getCurrency();
+        Set<NotificationChannel> preferredNotificationChannels = preference.getNotificationChannels();
+
+        if (preferredCurrency != null) {
+            userPreference.setCurrency(preference.getCurrency());
+        }
+
+        if (preferredNotificationChannels != null) {
+            userPreference.setNotificationChannels(preferredNotificationChannels);
+        }
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public void deleteUser(@NotNull final Long id) {
+        userRepository.deleteById(id);
     }
 }
