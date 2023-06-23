9ec174d0ed3efcfc7c7d0d30adc787cce9dfad41
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-23 18:55:24 +0200

    Added PaymentActionRepository.

---
+++ src/main/java/com/coinverse/api/common/repositories/PaymentActionRepository.java
@@ -0,0 +1,12 @@
+package com.coinverse.api.common.repositories;
+
+import com.coinverse.api.common.entities.PaymentAction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaymentActionRepository extends JpaRepository<PaymentAction, Long> {
+    Optional<PaymentAction> findByName(String name);
+}
