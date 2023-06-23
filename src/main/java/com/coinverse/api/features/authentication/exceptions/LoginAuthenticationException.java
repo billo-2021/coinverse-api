1368018e7a2826d800bd2f8955bf50c68c274b48
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-23 19:04:29 +0200

    Added login authentication exception.

---
+++ src/main/java/com/coinverse/api/features/authentication/exceptions/LoginAuthenticationException.java
@@ -0,0 +1,33 @@
+package com.coinverse.api.features.authentication.exceptions;
+
+import com.coinverse.api.common.models.AccountResponse;
+import com.coinverse.api.common.security.exceptions.ApiAuthenticationException;
+import com.coinverse.api.features.authentication.models.LoginRequest;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.core.AuthenticationException;
+
+public class LoginAuthenticationException extends ApiAuthenticationException {
+    private final LoginRequest request;
+    private final AccountResponse account;
+    private final AuthenticationException exception;
+
+    public LoginAuthenticationException(@NotNull final LoginRequest request,
+                                        @NotNull final AccountResponse account,
+                                        @NotNull final AuthenticationException exception) {
+        this.request = request;
+        this.account = account;
+        this.exception = exception;
+    }
+
+    public LoginRequest getRequest() {
+        return request;
+    }
+
+    public AccountResponse getAccount() {
+        return account;
+    }
+
+    public AuthenticationException getException() {
+        return exception;
+    }
+}
