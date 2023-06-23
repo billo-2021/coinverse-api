834e75d615f025ddbc93f1a87c1b6c7dcd2c5acc
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-23 19:03:33 +0200

    Added endpoints for authentication controller.

--- src/main/java/com/coinverse/api/features/authentication/controllers/AuthenticationController.java
+++ src/main/java/com/coinverse/api/features/authentication/controllers/AuthenticationController.java
@@ -1,17 +1,13 @@
 package com.coinverse.api.features.authentication.controllers;
 
-import com.coinverse.api.common.models.AuthenticationResponse;
-import com.coinverse.api.common.models.LoginRequest;
-import com.coinverse.api.common.models.RegisterRequest;
+import com.coinverse.api.common.models.UserResponse;
+import com.coinverse.api.features.authentication.models.*;
 import com.coinverse.api.features.authentication.services.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/authentication")
@@ -20,25 +16,44 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest registerRequest
+    public ResponseEntity<UserResponse> register(
+            @Valid @RequestBody final RegisterRequest registerRequest
     ) {
-        final AuthenticationResponse registrationResponse = authenticationService.register(registerRequest);
+        final UserResponse registrationResponse = authenticationService.register(registerRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(registrationResponse);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody LoginRequest loginRequest
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody final LoginRequest loginRequest
     ) {
-//        try {
-//            final AuthenticationResponse loginResponse = authenticationService.login(loginRequest);
-//            return ResponseEntity.ok(loginResponse);
-//        } catch (Exception exception) {
-//            System.out.println("Something went wrong" + exception);
-//        }
-//
-//        return ResponseEntity.ok(new AuthenticationResponse());
-        throw new BadCredentialsException("Bad credentials");
+        final LoginResponse loginResponse = authenticationService.login(loginRequest);
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/request-token")
+    public ResponseEntity<String> requestToken(@Valid @RequestBody final TokenRequest tokenRequest) {
+        authenticationService.requestToken(tokenRequest);
+        return ResponseEntity.ok("Account verification token sent");
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyAccount(
+            @Valid @RequestBody final TokenVerifyRequest verifyAccountRequest
+    ) {
+        authenticationService.verifyAccount(verifyAccountRequest);
+        return ResponseEntity.ok("Account verified");
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<String> requestResetPasswordToken(@Valid @RequestBody final TokenRequest tokenRequest) {
+        authenticationService.requestResetPasswordToken(tokenRequest);
+        return ResponseEntity.ok("Password token sent");
+    }
+
+    @PatchMapping("/reset-password")
+    public ResponseEntity<String> resetPassword(@Valid @RequestBody final ResetPasswordRequest resetPasswordRequest) {
+        authenticationService.resetPassword(resetPasswordRequest);
+        return ResponseEntity.ok("Account password reset");
     }
 }
