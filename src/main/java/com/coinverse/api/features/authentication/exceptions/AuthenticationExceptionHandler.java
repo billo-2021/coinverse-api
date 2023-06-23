0fdeb243204f0690a180f3a580b77088b9a7afdb
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-23 19:03:53 +0200

    Added authentication exception handler.

---
+++ src/main/java/com/coinverse/api/features/authentication/exceptions/AuthenticationExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.coinverse.api.features.authentication.exceptions;
+
+import com.coinverse.api.common.errors.ErrorResponse;
+import com.coinverse.api.common.models.AccountResponse;
+import com.coinverse.api.common.security.exceptions.AccountDisabledException;
+import com.coinverse.api.common.security.exceptions.AccountLockedException;
+import com.coinverse.api.common.security.exceptions.ApiAuthenticationException;
+import com.coinverse.api.common.security.exceptions.VerificationRequiredException;
+import com.coinverse.api.common.services.AccountService;
+import com.coinverse.api.common.utils.ExceptionUtil;
+import com.coinverse.api.features.authentication.models.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class AuthenticationExceptionHandler {
+    private final AccountService accountService;
+    private final PasswordEncoder passwordEncoder;
+
+    @ExceptionHandler(LoginAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(
+            final LoginAuthenticationException loginAuthEx) {
+        final LoginRequest loginRequest = loginAuthEx.getRequest();
+        final String password = loginRequest.getPassword();
+
+        final AccountResponse accountResponse = loginAuthEx.getAccount();
+        final Boolean isAccountEnabled = accountResponse.getIsEnabled();
+
+        final AuthenticationException authEx = loginAuthEx.getException();
+
+        final boolean passwordsMatch = passwordEncoder
+                .matches(password, accountResponse.getPassword());
+
+        accountService.addAccountLoginAttemptsById(accountResponse.getId());
+
+        if (passwordsMatch && authEx instanceof LockedException) {
+            return handleApiAuthenticationException(new AccountLockedException());
+        }
+
+        if (passwordsMatch && authEx instanceof DisabledException && isAccountEnabled) {
+            return handleApiAuthenticationException(new VerificationRequiredException());
+        }
+
+        if (passwordsMatch && authEx instanceof DisabledException) {
+            return handleApiAuthenticationException(new AccountDisabledException());
+        }
+
+        return handleApiAuthenticationException(new ApiAuthenticationException(authEx));
+    }
+
+    @ExceptionHandler(ApiAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleApiAuthenticationException(
+            final ApiAuthenticationException apiAuthEx) {
+        return ExceptionUtil.getApiErrorResponse(apiAuthEx);
+    }
+}
