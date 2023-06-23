d89c2005a5b45c6e4d7e9aeeeba21faf35217ca3
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-23 19:01:30 +0200

    Added DateTimeUtil

---
+++ src/main/java/com/coinverse/api/core/utils/DateTimeUtil.java
@@ -0,0 +1,9 @@
+package com.coinverse.api.core.utils;
+
+import java.time.OffsetDateTime;
+
+public class DateTimeUtil {
+    public static OffsetDateTime getCurrentTimeStamp() {
+        return OffsetDateTime.now();
+    }
+}
