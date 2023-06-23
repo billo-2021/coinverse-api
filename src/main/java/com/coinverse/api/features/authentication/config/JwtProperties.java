0f08ed7ea1ab5446d96a117ac2841639bb87b09d
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-06-23 19:03:02 +0200

    Added JwtProperties record.

--- src/main/java/com/coinverse/api/features/authentication/config/JwtProperties.java
+++ src/main/java/com/coinverse/api/features/authentication/config/JwtProperties.java
@@ -1,14 +1,10 @@
 package com.coinverse.api.features.authentication.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Data
-@Configuration
+
 @ConfigurationProperties("security.jwt")
-public class JwtProperties {
-    private String secretKey;
-
+public record JwtProperties(
+        String secretKey){
 
 }
