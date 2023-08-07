1cc512024ccac279c77948127ae7292c0b929510
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:20:20 +0200

    Added account routes.

---
+++ src/main/java/com/coinverse/api/common/config/routes/AccountRoutes.java
@@ -0,0 +1,7 @@
+package com.coinverse.api.common.config.routes;
+
+public class AccountRoutes {
+    public static final String PATH = BaseRoute.PATH + "/account";
+    public static final String CHANGE_PASSWORD = "/change-password";
+    public static final String ACTIVITY = "/activity";
+}
