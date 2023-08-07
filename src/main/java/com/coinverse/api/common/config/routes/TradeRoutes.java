10c8d1b4632bc8c238a1f1ef30bcc028f9d9bbfd
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:22:35 +0200

    Added trade routes.

---
+++ src/main/java/com/coinverse/api/common/config/routes/TradeRoutes.java
@@ -0,0 +1,6 @@
+package com.coinverse.api.common.config.routes;
+
+public class TradeRoutes {
+    public static final String PATH = BaseRoute.PATH + "/trades";
+    public static final String GET_TRADE_BY_ID = "/{id}";
+}
