d4420255e95b667be074ce7bc9648ef09f4232fc
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:25:04 +0200

    Added query for searching a crypto currency by currency code or currency name.

--- src/main/java/com/coinverse/api/common/repositories/CryptoCurrencyRepository.java
+++ src/main/java/com/coinverse/api/common/repositories/CryptoCurrencyRepository.java
@@ -1,7 +1,10 @@
 package com.coinverse.api.common.repositories;
 
 import com.coinverse.api.common.entities.CryptoCurrency;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,11 @@ import java.util.Optional;
 @Repository
 public interface CryptoCurrencyRepository extends JpaRepository<CryptoCurrency, Long> {
     Optional<CryptoCurrency> findByCurrencyCodeIgnoreCase(String currencyCode);
+    @Query(value = """
+            SELECT cryptoCurrency
+            FROM CryptoCurrency cryptoCurrency
+            WHERE lower(cryptoCurrency.currency.code) LIKE CONCAT('%', lower(:query), '%')
+            OR lower(cryptoCurrency.currency.name) LIKE CONCAT('%', lower(:query), '%')
+            """)
+    Page<CryptoCurrency> search(String query, Pageable pageable);
 }
