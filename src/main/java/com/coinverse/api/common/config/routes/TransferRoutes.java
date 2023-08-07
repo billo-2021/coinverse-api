3a0189cf22663221fefe2c2edb230be2d0b64700
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:22:47 +0200

    Added transfer routes.

---
+++ src/main/java/com/coinverse/api/common/config/routes/TransferRoutes.java
@@ -0,0 +1,9 @@
+package com.coinverse.api.common.config.routes;
+
+public class TransferRoutes {
+    public static final String PATH = BaseRoute.PATH + "/transfers";
+    public static final String DEPOSIT = "/deposit";
+    public static final String WITHDRAW = "/withdraw";
+    public static final String TRANSACTIONS = "/transactions";
+    public static final String GET_TRANSACTION_BY_ID = "/transactions/{id}";
+}
