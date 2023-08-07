7371316c804c21090adb053a63d198d772be67f2
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:21:21 +0200

    Added balance routes.

---
+++ src/main/java/com/coinverse/api/common/config/routes/BalanceRoutes.java
@@ -0,0 +1,7 @@
+package com.coinverse.api.common.config.routes;
+
+public class BalanceRoutes {
+    public static final String PATH = BaseRoute.PATH + "/balances";
+    public static final String ALL_BALANCES = "/all";
+    public static final String GET_BALANCE_BY_ID = "/{id}";
+}
