abacbfedf8914d3f63cd3bff896b20e411981b96
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:22:26 +0200

    Added quote routes.

---
+++ src/main/java/com/coinverse/api/common/config/routes/QuoteRoutes.java
@@ -0,0 +1,8 @@
+package com.coinverse.api.common.config.routes;
+
+public class QuoteRoutes {
+    public static final String PATH = BaseRoute.PATH + "/quotes";
+    public static final String CURRENCY_PAIR = "/currency-pair";
+    public static final String CRYPTO = "/crypto";
+    public static final String FOREX = "/forex";
+}
