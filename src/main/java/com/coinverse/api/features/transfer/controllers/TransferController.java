81be5bd4ecae91e682cfe886a8e145d8f934da5c
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:50:24 +0200

    Updated TransferController, refactoring.

--- src/main/java/com/coinverse/api/features/transfer/controllers/TransferController.java
+++ src/main/java/com/coinverse/api/features/transfer/controllers/TransferController.java
@@ -1,5 +1,6 @@
 package com.coinverse.api.features.transfer.controllers;
 
+import com.coinverse.api.common.config.routes.TransferRoutes;
 import com.coinverse.api.common.constants.PageConstants;
 import com.coinverse.api.common.models.PageResponse;
 import com.coinverse.api.common.validators.PageRequestValidator;
@@ -10,29 +11,25 @@ import com.coinverse.api.features.transfer.services.TransferService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(TransferController.PATH)
+@RequestMapping(TransferRoutes.PATH)
 @RequiredArgsConstructor
 public class TransferController {
-    public static final String PATH = "/api/v1/transfers";
     private final TransferService transferService;
 
-    @PostMapping("/deposit")
-    public ResponseEntity<PaymentResponse> deposit(@Valid @RequestBody DepositRequest depositRequest) {
-        final PaymentResponse paymentResponse = transferService.deposit(depositRequest);
-        return ResponseEntity.ok(paymentResponse);
+    @PostMapping(TransferRoutes.DEPOSIT)
+    public PaymentResponse deposit(@Valid @RequestBody DepositRequest depositRequest) {
+        return transferService.deposit(depositRequest);
     }
 
-    @PostMapping("/withdraw")
-    public ResponseEntity<PaymentResponse> withdraw(@Valid @RequestBody WithdrawRequest withdrawRequest) {
-        final PaymentResponse paymentResponse = transferService.withdraw(withdrawRequest);
-        return ResponseEntity.ok(paymentResponse);
+    @PostMapping(TransferRoutes.WITHDRAW)
+    public PaymentResponse withdraw(@Valid @RequestBody WithdrawRequest withdrawRequest) {
+        return transferService.withdraw(withdrawRequest);
     }
 
-    @GetMapping("/transactions")
+    @GetMapping(TransferRoutes.TRANSACTIONS)
     public PageResponse<PaymentResponse> getTransactions(@RequestParam(value = "pageNumber", defaultValue = PageConstants.DEFAULT_PAGE, required = false) int pageNumber,
                                                                          @RequestParam(value = "pageSize", defaultValue = PageConstants.DEFAULT_SIZE, required = false) int pageSize,
                                                                          @RequestParam(value = "sortBy", defaultValue = PageConstants.DEFAULT_SORT_BY, required = false) String sortBy,
@@ -41,7 +38,7 @@ public class TransferController {
         return transferService.getTransactions(pageable);
     }
 
-    @GetMapping("/transactions/{id}")
+    @GetMapping(TransferRoutes.GET_TRANSACTION_BY_ID)
     public PaymentResponse getTransactionById(@PathVariable Long id) {
         return transferService.getTransactionById(id);
     }
