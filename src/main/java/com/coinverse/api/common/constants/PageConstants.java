443c834ff557bceda013f7df3f8050a44d3ae0fb
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 02:22:00 +0200

    Added Page constants.

---
+++ src/main/java/com/coinverse/api/common/constants/PageConstants.java
@@ -0,0 +1,8 @@
+package com.coinverse.api.common.constants;
+
+public class PageConstants {
+    public static final String DEFAULT_PAGE = "0";
+    public static final String DEFAULT_SIZE = "100";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
+}
