4a29c9796c575194567125a99c6829a72856da03
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:24:00 +0200

    Updated ApiMessage response, added of method for creating a message response.

--- src/main/java/com/coinverse/api/common/models/ApiMessageResponse.java
+++ src/main/java/com/coinverse/api/common/models/ApiMessageResponse.java
@@ -1,5 +1,6 @@
 package com.coinverse.api.common.models;
 
+import com.coinverse.api.common.constants.ApiMessage;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,4 +10,8 @@ import lombok.Data;
 @Builder
 public class ApiMessageResponse {
     private String message;
+
+    public static ApiMessageResponse of(ApiMessage apiMessageEnum) {
+        return new ApiMessageResponse(apiMessageEnum.getMessage());
+    }
 }
