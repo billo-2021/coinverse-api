14fe9b860975e0fc5486e1624350de11eba23a55
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:22:15 +0200

    Added profile routes.

---
+++ src/main/java/com/coinverse/api/common/config/routes/ProfileRoutes.java
@@ -0,0 +1,8 @@
+package com.coinverse.api.common.config.routes;
+
+public class ProfileRoutes {
+    public static final String PATH = BaseRoute.PATH + "/profile";
+    public static final String PERSONAL_INFORMATION = "/personal-information";
+    public static final String ADDRESS = "/address";
+    public static final String PREFERENCE = "/preference";
+}
