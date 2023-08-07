784854f169536825978098fced32687331e82164
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 02:21:28 +0200

    Added base route.

---
+++ src/main/java/com/coinverse/api/common/config/routes/BaseRoute.java
@@ -0,0 +1,6 @@
+package com.coinverse.api.common.config.routes;
+
+public class BaseRoute {
+    public final static String VERSION = "/v1";
+    public final static String PATH =  "/api" + BaseRoute.VERSION;
+}
