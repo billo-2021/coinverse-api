dc57125d7be9b9b5d5386e7b41a0ca2a3a882861
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:22:05 +0200

    Updated lookup routes to make make static access modifier before final.

--- src/main/java/com/coinverse/api/common/config/routes/LookupRoutes.java
+++ src/main/java/com/coinverse/api/common/config/routes/LookupRoutes.java
@@ -1,18 +1,18 @@
 package com.coinverse.api.common.config.routes;
 
 public class LookupRoutes {
-    public final static String PATH = BaseRoute.PATH + "/lookup";
-    public final static String COUNTRIES = "/countries";
-    public final static String All_COUNTRIES = "/all/countries";
-    public final static String CURRENCIES = "/currencies";
-    public final static String ALL_CURRENCIES = "/all/currencies";
-    public final static String CRYPTO = "/crypto";
-    public final static String ALL_CRYPTO = "/all/crypto";
-    public final static String CRYPTO_BY_CURRENCY_CODE = "/crypto/{currencyCode}";
-    public final static String CURRENCY_PAIRS = "/currency-pairs";
-    public final static String ALL_CURRENCY_PAIRS = "/all/currency-pairs";
-    public final static String NOTIFICATION_METHODS = "/notification-methods";
-    public final static String ALL_NOTIFICATION_METHODS = "/all/notification-methods";
-    public final static String PAYMENT_METHODS = "/payment-methods";
-    public final static String ALL_PAYMENT_METHODS = "/all/payment-methods";
+    public static final String PATH = BaseRoute.PATH + "/lookup";
+    public static final String COUNTRIES = "/countries";
+    public static final String All_COUNTRIES = "/all/countries";
+    public static final String CURRENCIES = "/currencies";
+    public static final String ALL_CURRENCIES = "/all/currencies";
+    public static final String CRYPTO = "/crypto";
+    public static final String ALL_CRYPTO = "/all/crypto";
+    public static final String CRYPTO_BY_CURRENCY_CODE = "/crypto/{currencyCode}";
+    public static final String CURRENCY_PAIRS = "/currency-pairs";
+    public static final String ALL_CURRENCY_PAIRS = "/all/currency-pairs";
+    public static final String NOTIFICATION_METHODS = "/notification-methods";
+    public static final String ALL_NOTIFICATION_METHODS = "/all/notification-methods";
+    public static final String PAYMENT_METHODS = "/payment-methods";
+    public static final String ALL_PAYMENT_METHODS = "/all/payment-methods";
 }
