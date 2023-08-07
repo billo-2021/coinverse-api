35be3af24b89225b4792341a88f0c2b45e100559
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 02:38:29 +0200

    Added trade request model.

--- src/main/java/com/coinverse/api/features/trade/models/TradeRequest.java
+++ src/main/java/com/coinverse/api/features/trade/models/TradeRequest.java
@@ -1,5 +1,11 @@
 package com.coinverse.api.features.trade.models;
 
+import com.coinverse.api.common.models.CurrencyTransactionActionEnum;
+import com.coinverse.api.common.validators.EnumValidator;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,8 +14,16 @@ import lombok.Data;
 @Data
 @Builder
 public class TradeRequest {
+    @DecimalMin(value = "1.00", message = "amount must be equal or greater than 1.00")
+    @DecimalMax(value = "10000000", message = "amount must be equal or less than 10000000")
     private double amount;
+
+    @NotBlank(message = "amountCurrencyCode is required")
     private String amountCurrencyCode;
+
+    @NotNull(message = "quoteId is required")
     private Long quoteId;
+
+    @EnumValidator(message = "action is required", target = CurrencyTransactionActionEnum.class)
     private String action;
 }
