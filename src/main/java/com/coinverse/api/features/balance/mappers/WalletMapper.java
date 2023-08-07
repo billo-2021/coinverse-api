8fb20d8b10d76007caf4422782c3f4cc414e0138
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 02:38:40 +0200

    Added wallet mapper.

--- src/main/java/com/coinverse/api/features/balance/mappers/WalletMapper.java
+++ src/main/java/com/coinverse/api/features/balance/mappers/WalletMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface WalletMapper {
-    WalletResponse waletToWalletResponse(Wallet wallet);
+    WalletResponse walletToWalletResponse(Wallet wallet);
 }
