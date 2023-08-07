b1ff7049982b7658fd44b2671730d1b968acd4fd
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 02:21:45 +0200

    Added lookup routes.

---
+++ src/main/java/com/coinverse/api/common/config/routes/LookupRoutes.java
@@ -0,0 +1,18 @@
+package com.coinverse.api.common.config.routes;
+
+public class LookupRoutes {
+    public final static String PATH = BaseRoute.PATH + "/lookup";
+    public final static String COUNTRIES = "/countries";
+    public final static String All_COUNTRIES = "/all/countries";
+    public final static String CURRENCIES = "/currencies";
+    public final static String ALL_CURRENCIES = "/all/currencies";
+    public final static String CRYPTO = "/crypto";
+    public final static String ALL_CRYPTO = "/all/crypto";
+    public final static String CRYPTO_BY_CURRENCY_CODE = "/crypto/{currencyCode}";
+    public final static String CURRENCY_PAIRS = "/currency-pairs";
+    public final static String ALL_CURRENCY_PAIRS = "/all/currency-pairs";
+    public final static String NOTIFICATION_METHODS = "/notification-methods";
+    public final static String ALL_NOTIFICATION_METHODS = "/all/notification-methods";
+    public final static String PAYMENT_METHODS = "/payment-methods";
+    public final static String ALL_PAYMENT_METHODS = "/all/payment-methods";
+}
