6055833c8f1f54bf3e087edb79ea0b6b7e854795
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:23:10 +0200

    Added header constants.

---
+++ src/main/java/com/coinverse/api/common/constants/HeaderConstants.java
@@ -0,0 +1,6 @@
+package com.coinverse.api.common.constants;
+
+public class HeaderConstants {
+    public static final String USER_AGENT = "user-agent";
+    public static final String FROM = "from";
+}
