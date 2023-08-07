83c45b549af35e8c235d5e472247fe534ac04db5
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:25:47 +0200

    Updated UserAccount service, refactoring remove @NotNull and final annotations.

--- src/main/java/com/coinverse/api/common/security/services/UserAccountService.java
+++ src/main/java/com/coinverse/api/common/security/services/UserAccountService.java
@@ -1,11 +1,11 @@
 package com.coinverse.api.common.security.services;
 
 import com.coinverse.api.common.security.models.UserAccount;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Optional;
 
 public interface UserAccountService extends UserDetailsService {
-    Optional<UserAccount> getUserAccountById(final @NotNull Long id);
+    Optional<UserAccount> getUserAccountById(Long id);
+    UserAccount getCurrentUser();
 }
