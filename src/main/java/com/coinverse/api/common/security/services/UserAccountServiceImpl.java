c74c4ec87a6ab4252d08113130fa4357c9da3f90
billo-2021 <89088739+billo-2021@users.noreply.github.com>
2023-08-07 18:26:34 +0200

    Updated UserAccountService, added the getCurrentUser method.

--- src/main/java/com/coinverse/api/common/security/services/UserAccountServiceImpl.java
+++ src/main/java/com/coinverse/api/common/security/services/UserAccountServiceImpl.java
@@ -4,8 +4,8 @@ import com.coinverse.api.common.repositories.AccountRepository;
 import com.coinverse.api.common.security.exceptions.UserAccountNotFoundException;
 import com.coinverse.api.common.security.mappers.UserAccountMapper;
 import com.coinverse.api.common.security.models.UserAccount;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -19,14 +19,20 @@ public class UserAccountServiceImpl implements UserAccountService {
     private final UserAccountMapper userAccountMapper;
 
     @Override
-    public UserDetails loadUserByUsername(final @NotNull String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return accountRepository.findByUsernameIgnoreCase(username)
                 .map(userAccountMapper::accountToUserAccount)
                 .orElseThrow(UserAccountNotFoundException::new);
     }
 
     @Override
-    public Optional<UserAccount> getUserAccountById(final @NotNull Long id) {
+    public Optional<UserAccount> getUserAccountById(Long id) {
         return Optional.empty();
+    }
+
+    @Override
+    public UserAccount getCurrentUser() {
+        return (UserAccount) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
     }
 }
